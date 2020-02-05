4b17b5673cb110cd91c3dc6ac8662e937764aa19
S7K <sala7khaled.s7k@gmail.com>
2020-02-05 02:21:27 +0200

    Percentage Label added

--- Egg Timer/ViewController.swift
+++ Egg Timer/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var progressBar: UIProgressView!
     @IBOutlet weak var titleLabel: UILabel!
+    
     let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
     var timer = Timer()
     var player: AVAudioPlayer!
